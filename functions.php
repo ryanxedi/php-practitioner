b03bcb0adb47fce68ce5758a368d4707f1821982
Ryan Howard <ryan@xedi.com>
2018-12-13 21:48:13 +0000

    Securfed the config

--- functions.php
+++ functions.php
@@ -1,34 +1,5 @@
 <?php 
 
-function connectToDb()
-{
-		try {
-		
-		return new PDO('mysql:host=127.0.0.1;dbname=mytodo', 'root', '');
-
-	} catch (PDOException $e) {
-
-		die($e->getMessage());
-
-	}	
-}
-
-
-function fetchAllTasks($pdo)
-{
-	$statement = $pdo->prepare('select * from todos');
-	$statement->execute();
-	return $statement->fetchAll(PDO::FETCH_CLASS, 'Task');
-}
-
 function dd($data) {
 	die(var_dump ($data));
-}
-
-function age($age) {
-	if ($age >= 21) :
-		echo 'Come on in'; 
-	else : 
-		echo 'You are not old enough';
-	endif;
 }
